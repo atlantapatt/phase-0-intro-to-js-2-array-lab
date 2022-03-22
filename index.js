f8cf793256dc8e7727cb16a74968bebd2a018109
Atlanta <atlantarpatterson@gmail.com>
2022-03-22 11:14:06 -0700

    commit tree 451c4b8c145756d68054c54dd198d5deed90a518

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name) {
+    return cats.push(name)
+}
+
+function destructivelyPrependCat(name) {
+    return cats.unshift(name)
+}
+
+function destructivelyRemoveLastCat() {
+    return cats.splice([2])
+}
+
+function destructivelyRemoveFirstCat() {
+    return cats.splice([0],[1])
+}
+
+function appendCat(name) {
+    let appenedCats = [...cats, name]
+    return appenedCats
+}
+
+function prependCat(name) {
+    let prependCat = [name, ...cats]
+    return prependCat
+}
+
+function removeLastCat() {
+    let removedCat = [...cats.slice([0], [2])]
+    return removedCat
+}
+
+function removeFirstCat() {
+    let removedFirstCat = [...cats.slice([1])]
+    return removedFirstCat
+}
